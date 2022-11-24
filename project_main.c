b36265021da541bcb7d3274065bc10ecadcdaf7f
Omer Kriger <83721785+OmerKriger@users.noreply.github.com>
2022-11-24 17:15:34 +0200

    Update project_main.c
    removing personal information

--- project_main.c
+++ project_main.c
@@ -1,18 +1,5 @@
 #include <stdlib.h>
 
-/*
-Nir Peretz, ID: 318382850
-Omer Kriger, ID: 207752536
-*/
-
-/*
-The program is a system for planning concerts
-The program gets 2 files, first file is instruments, second file is Musicians.
-The program take the instrument file and make a tree of instrument sorted lexicographic
-and make from musicians files array with all the details about the musicians like his name, instruments
-get from the user requires for a concert and if there are resources (musicians) for the concert it print the details
-*/
-
 // Includes
 #include "mainh.h"
 #include "trees.h"
